1b1f959e9a8b7e77e5b1d4cb6159ca17f97b6491
Goutham_WGC <gouthamkumar@willgibbins.com>
2026-09-08 08:53:57 +0530

    Bug fixes in the navbar and herosection

--- client/assets/LegacyCatalogRedirect-DZHHiAjI.js
+++
@@ -1,1 +0,0 @@
-import{e as r,aL as c,j as n,aM as i}from"./index-B3ba86WN.js";function p({toSuffix:a}){const e=r(),t=c(),s=e.pathname.split("/").slice(0,2).join("/"),o=t.categoryId?a.replace(":categoryId",t.categoryId):a;return n.jsx(i,{to:`${s}/${o}`,replace:!0,state:e.state})}export{p as default};
